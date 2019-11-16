b22d8072a6d7952226f17873c0966bc45ba9f833
GIJedi <mail.nathanrose@gmail.com>
2019-11-16 12:11:02 -0500

    Developed and tested the data memory module

--- sources_1/imports/BRISC/ssegx8.v
+++ sources_1/imports/BRISC/ssegx8.v
@@ -21,6 +21,7 @@
 
 module ssegx8(
     input CLK,
+    input debug,
     input [31:0] VALUE,
     output reg [7:0] SSEG_CA,
     output reg [7:0] SSEG_AN
@@ -29,63 +30,121 @@ reg [20:0] digit_counter;
 reg [3:0] digits;
 always @(posedge CLK)
 begin
-	digit_counter<=digit_counter+1;
-	case (digits)
-				    // pgfedcba
-		0:SSEG_CA = 8'b11000000;
-		1:SSEG_CA = 8'b11111001;
-		2:SSEG_CA = 8'b10100100; 
-		3:SSEG_CA = 8'b10110000;
-		4:SSEG_CA = 8'b10011001; 
-		5:SSEG_CA = 8'b10010010; 
-		6:SSEG_CA = 8'b10000010; 
-		7:SSEG_CA = 8'b11111000; 
-		8:SSEG_CA = 8'b10000000;
-		9:SSEG_CA = 8'b10010000;
-		10:SSEG_CA=~8'b11110111; 
-		11:SSEG_CA=~8'b11111100; 
-		12:SSEG_CA=~8'b10111001; 
-		13:SSEG_CA=~8'b11011110; 
-		14:SSEG_CA=~8'b11111001; 
-		15:SSEG_CA=~8'b11110001; 
-		
-	 endcase
- 
-	case (digit_counter[16:14])
-		0 : begin
-				SSEG_AN=8'b01111111;
-				digits<=VALUE[31:28];
-			end
-		1 : begin
-				SSEG_AN=8'b10111111;
-				digits<=VALUE[27:24];
-			end
-		2 : begin
-				SSEG_AN=8'b11011111;
-				digits<=VALUE[23:20];
-			end
-		3 : begin
-				SSEG_AN=8'b11101111;
-				digits<=VALUE[19:16];
-			end		
-        4 : begin
-                SSEG_AN=8'b11110111;
-                digits<=VALUE[15:12];
-            end
-        5 : begin
-                SSEG_AN=8'b11111011;
-                digits<=VALUE[11:8];
-            end
-        6 : begin
-                SSEG_AN=8'b11111101;
-                digits<=VALUE[7:4];
-            end
-        7 : begin
-                SSEG_AN=8'b11111110;
-                digits<=VALUE[3:0];
-            end        		
-	endcase	
-
+    //the debug is so the CA and AN update on every pulse
+    if(debug == 1) begin
+	   digit_counter<=digit_counter+1;
+	   case (digits)
+                        // pgfedcba
+            0:SSEG_CA = 8'b11000000;
+            1:SSEG_CA = 8'b11111001;
+            2:SSEG_CA = 8'b10100100; 
+            3:SSEG_CA = 8'b10110000;
+            4:SSEG_CA = 8'b10011001; 
+            5:SSEG_CA = 8'b10010010; 
+            6:SSEG_CA = 8'b10000010; 
+            7:SSEG_CA = 8'b11111000; 
+            8:SSEG_CA = 8'b10000000;
+            9:SSEG_CA = 8'b10010000;
+            10:SSEG_CA=~8'b11110111; 
+            11:SSEG_CA=~8'b11111100; 
+            12:SSEG_CA=~8'b10111001; 
+            13:SSEG_CA=~8'b11011110; 
+            14:SSEG_CA=~8'b11111001; 
+            15:SSEG_CA=~8'b11110001;    
+        endcase
+    
+        case (digit_counter[2:0])
+            0 : begin
+                    SSEG_AN=8'b01111111;
+                    digits<=VALUE[31:28];
+                end
+            1 : begin
+                    SSEG_AN=8'b10111111;
+                    digits<=VALUE[27:24];
+                end
+            2 : begin
+                    SSEG_AN=8'b11011111;
+                    digits<=VALUE[23:20];
+                end
+            3 : begin
+                    SSEG_AN=8'b11101111;
+                    digits<=VALUE[19:16];
+                end		
+            4 : begin
+                    SSEG_AN=8'b11110111;
+                    digits<=VALUE[15:12];
+                end
+            5 : begin
+                    SSEG_AN=8'b11111011;
+                    digits<=VALUE[11:8];
+                end
+            6 : begin
+                    SSEG_AN=8'b11111101;
+                    digits<=VALUE[7:4];
+                end
+            7 : begin
+                    SSEG_AN=8'b11111110;
+                    digits<=VALUE[3:0];
+                end        		
+            endcase	
+        end
+        else begin
+            digit_counter<=digit_counter+1;
+	        case (digits)
+                        // pgfedcba
+            0:SSEG_CA = 8'b11000000;
+            1:SSEG_CA = 8'b11111001;
+            2:SSEG_CA = 8'b10100100; 
+            3:SSEG_CA = 8'b10110000;
+            4:SSEG_CA = 8'b10011001; 
+            5:SSEG_CA = 8'b10010010; 
+            6:SSEG_CA = 8'b10000010; 
+            7:SSEG_CA = 8'b11111000; 
+            8:SSEG_CA = 8'b10000000;
+            9:SSEG_CA = 8'b10010000;
+            10:SSEG_CA=~8'b11110111; 
+            11:SSEG_CA=~8'b11111100; 
+            12:SSEG_CA=~8'b10111001; 
+            13:SSEG_CA=~8'b11011110; 
+            14:SSEG_CA=~8'b11111001; 
+            15:SSEG_CA=~8'b11110001;    
+        endcase
+    
+        case (digit_counter[16:14])
+            0 : begin
+                    SSEG_AN=8'b01111111;
+                    digits<=VALUE[31:28];
+                end
+            1 : begin
+                    SSEG_AN=8'b10111111;
+                    digits<=VALUE[27:24];
+                end
+            2 : begin
+                    SSEG_AN=8'b11011111;
+                    digits<=VALUE[23:20];
+                end
+            3 : begin
+                    SSEG_AN=8'b11101111;
+                    digits<=VALUE[19:16];
+                end		
+            4 : begin
+                    SSEG_AN=8'b11110111;
+                    digits<=VALUE[15:12];
+                end
+            5 : begin
+                    SSEG_AN=8'b11111011;
+                    digits<=VALUE[11:8];
+                end
+            6 : begin
+                    SSEG_AN=8'b11111101;
+                    digits<=VALUE[7:4];
+                end
+            7 : begin
+                    SSEG_AN=8'b11111110;
+                    digits<=VALUE[3:0];
+                end        		
+            endcase	
+        end
 end
 endmodule
 
---
+++ sources_1/new/data_mem.v
@@ -0,0 +1,64 @@
+
+
+
+module data_mem (
+        //control values
+        input CLK,
+        input [7:0] address,
+        input read_en,
+        input write_en,
+        input [15:0] input_data,
+        output reg [15:0] output_data,
+        
+        //values for IO hooked up to data memory
+        input [15:0] SW,
+        input [4:0] BTNS,
+        output [7:0] CA, AN,
+        output [15:0] LED,
+        
+        //debug variables
+        input debug_mode
+    );
+    
+    reg [7:0] memory[255:0];
+    
+    
+    //output stuff
+    ssegx8 sevenSegment(
+        .CLK(CLK),
+        .VALUE({memory[40], memory[41], memory[42], memory[43]}),
+        .SSEG_CA(CA),
+        .SSEG_AN(AN)
+    );
+    assign LED = {memory[8'h44], memory[8'h45]};
+    
+    always@(*) begin
+        //input stuff
+        memory[8'h4E] <= SW[15:8];
+        memory[8'h4F] <= SW[7:0];
+        memory[8'h50] <= BTNS;
+        
+        if(read_en) begin
+            output_data[15:8] <= memory[address];
+            output_data[7:0] <= memory[address + 1];
+        end
+        //Only a read operation or a write operation can be performed at
+        //any given time
+        else if(write_en) begin
+            
+            //accounts for read only sections of memory
+            case(address)
+            8'h4E:;
+            8'h4F:;
+            8'h50:;
+            //write allowed here
+            default: begin
+                    //you can write two bytes at at time!
+                    //but only two bytes at a time, so be careful.......
+                    memory[address] <= input_data[15:8];
+                    memory[address + 1] <= input_data[7:0];
+                    end
+            endcase
+        end
+    end
+endmodule
